6417f29d75ea5b0aac3ded869f77ccefe72a3882
KellyNatali <105806261+KellyNatali@users.noreply.github.com>
2022-06-23 21:49:55 -0500

    Envio de Header
    encabezado

---
+++ header1.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include('db.php');
+$header_html = '<html>
+    <head>
+        <title>SIN - 2022-I</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </head>
+    <body>';
+    if(isset($_SESSION["user_connected_id"])){
+      $sql = "SELECT * FROM cliente where id_Cliente = '".$_SESSION["user_connected_id"]."'";
+      $result = mysqli_query($con, $sql);
+      $user = mysqli_fetch_array($result);
+      echo "Hola:  ". $user['CI_Nombre'];
+      echo "&nbsp; ";
+      echo "<a href='http://localhost/www/logout.php'>Cerrar sesión</a>";
+    }else{
+      echo "<a href='http://localhost/www/login.php'>Ingresar</a>";
+    }
+
+    if(isset($_SESSION["cart"]) && count($_SESSION["cart"]) > 0){
+      echo "&nbsp; | &nbsp; <a href='http://localhost/www/cart.php'><span class='badge bg-secondary'>".count($_SESSION["cart"])."</span>&nbsp;Ir al carrito</a>";
+    }
+$footer_html ='</body>
+  </html>';
+?>
